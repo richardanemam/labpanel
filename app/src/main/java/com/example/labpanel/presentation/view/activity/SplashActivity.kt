bb6ede326b9a24bde9b6c1521c2c44c7f2a26c8c
Richard <richardanemam@gmail.com>
2020-04-28 16:13:26 -0300

    layout da splash screen

--- app/src/main/java/com/example/labpanel/presentation/view/activity/SplashActivity.kt
+++ app/src/main/java/com/example/labpanel/presentation/view/activity/SplashActivity.kt
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.labpanel.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
