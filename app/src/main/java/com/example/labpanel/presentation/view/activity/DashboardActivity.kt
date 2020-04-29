710712ffa0241f622818bba6c8125c3e23d41243
Richard <richardanemam@gmail.com>
2020-04-28 23:50:04 -0300

    cria dashboard

---
+++ app/src/main/java/com/example/labpanel/presentation/view/activity/DashboardActivity.kt
@@ -0,0 +1,51 @@
+package com.example.labpanel.presentation.view.activity
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.labpanel.R
+
+class DashboardActivity: AppCompatActivity() {
+
+    private lateinit var btnProfessor: Button
+    private lateinit var btnAluno: Button
+    private lateinit var btnSobreApp: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+        initViews()
+    }
+
+    private fun initViews() {
+        btnProfessor = findViewById(R.id.btn_dashboard_professor)
+        btnAluno = findViewById(R.id.btn_dashboard_aluno)
+        btnSobreApp = findViewById(R.id.btn_dashboard_sobre_app)
+
+        initProfessorButton()
+        initAlunoButton()
+        initSobreAppButton()
+    }
+
+    private fun initProfessorButton() {
+        btnProfessor.setOnClickListener {
+            //TODO integrate with loggin screen
+            Toast.makeText(this@DashboardActivity, "TODO integrate with loggin screen", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun initAlunoButton() {
+        btnAluno.setOnClickListener {
+            //TODO integrate with opportunities screen
+            Toast.makeText(this@DashboardActivity, "TODO integrate with opportunities screen", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun initSobreAppButton() {
+        btnSobreApp.setOnClickListener {
+            //TODO integrate with about the app screen
+            Toast.makeText(this@DashboardActivity, "TODO integrate with about the app screen", Toast.LENGTH_LONG).show()
+        }
+    }
+}
--- app/src/main/java/com/example/labpanel/presentation/view/activity/SplashActivity.kt
+++ app/src/main/java/com/example/labpanel/presentation/view/activity/SplashActivity.kt
@@ -1,9 +1,9 @@
 package com.example.labpanel.presentation.view.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.labpanel.R
@@ -34,7 +34,7 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun goToDashboardActivity() {
-       Toast.makeText(this, "SPLASH SCREEN FUNCIONANDO", Toast.LENGTH_LONG).show()
         finish()
+        startActivity(Intent(this, DashboardActivity::class.java))
     }
 }
