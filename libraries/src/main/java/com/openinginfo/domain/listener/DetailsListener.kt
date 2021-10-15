a6907d58d62331f566ab21edb8aea5717db3b03e
Richard <richardanemam@gmail.com>
2021-10-15 18:26:31 -0300

    correções no módulo de libs

--- libraries/src/main/java/com/openinginfo/domain/listener/DetailsListener.kt
+++ libraries/src/main/java/com/openinginfo/domain/listener/DetailsListener.kt
@@ -1,6 +1,7 @@
 package com.openinginfo.domain.listener
 
-import com.labpanel.feature.common.domain.model.OpeningsDataModel
+import com.openinginfo.domain.model.OpeningsDataModel
+
 
 interface DetailsListener {
     fun onClickDetailsButton(openingInfo: OpeningsDataModel)
