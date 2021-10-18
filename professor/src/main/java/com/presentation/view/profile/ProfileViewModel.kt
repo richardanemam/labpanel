6646410de1972aca1408555a9a3aa4e1347b29c9
Richard <richardanemam@gmail.com>
2021-10-18 15:10:53 -0300

    add tela de perfil do módulo de professor

---
+++ professor/src/main/java/com/presentation/view/profile/ProfileViewModel.kt
@@ -0,0 +1,53 @@
+package com.labpanel.feature.professor.presentation.view.profile
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.labpanel.feature.common.presentation.view.viewevents.LoadingState
+import com.labpanel.feature.professor.data.professorrepository.ProfessorRepository
+import com.labpanel.feature.professor.domain.states.OpeningsState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.lang.StringBuilder
+import java.util.*
+
+class ProfileViewModel(val repository: ProfessorRepository) : ViewModel() {
+
+    companion object {
+        private const val FIRST_POSITION = 0
+    }
+
+    private val openingsState: MutableLiveData<OpeningsState> = MutableLiveData()
+    val onOpeningsState: LiveData<OpeningsState> = openingsState
+
+    private val loadingState: MutableLiveData<LoadingState> = MutableLiveData()
+    val onLoadingState: LiveData<LoadingState> = loadingState
+
+    fun getOpenings() {
+        viewModelScope.launch(Dispatchers.IO) {
+            loadingState.postValue(LoadingState.Show)
+            repository.fetchOpenings(openingsState)
+        }.invokeOnCompletion {
+            loadingState.postValue(LoadingState.Hide)
+        }
+    }
+
+    fun getInitials(name: String): String {
+        val builder = StringBuilder()
+        val nameParts = name.split(" ").toTypedArray()
+
+        if (nameParts.size > 1) {
+            return builder
+                .append(nameParts[FIRST_POSITION][FIRST_POSITION].toString())
+                .append(nameParts[nameParts.size - 1][FIRST_POSITION].toString())
+                .toString()
+                .toUpperCase(Locale.ROOT)
+        }
+
+        return builder
+            .append(nameParts[FIRST_POSITION][FIRST_POSITION].toString())
+            .toString()
+            .toUpperCase(Locale.ROOT)
+    }
+}
