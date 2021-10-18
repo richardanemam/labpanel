6f80beef189a4703cc7ebf0cc798bbb931206c20
Richard <richardanemam@gmail.com>
2021-10-18 11:00:01 -0300

    cria módulo de professor e pacote de di do módulo

---
+++ professor/src/main/java/com/di/ProfessorModule.kt
@@ -0,0 +1,26 @@
+package com.di
+
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+val professorModule: Module = module {
+
+    single { Firebase.auth }
+    single { FirebaseDatabase.getInstance() }
+    single { FirebaseDatabase.getInstance().getReference("RegisteredOpenings") }
+    single { ProfessorRepository(auth = get(), databaseReference = get()) }
+
+    viewModel {
+        ProfileViewModel(
+            repository = get()
+        )
+    }
+
+    viewModel {
+        OpeningRegistrationViewModel(repository = get())
+    }
+}
